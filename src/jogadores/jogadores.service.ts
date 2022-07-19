00b89a5aabea49416b1d61b7d1e217b956594f8b
RonaldoMessias <ronaldo.messias@kabum.com.br>
2022-07-19 16:17:14 -0300

    [feat] - added new lib sqlite3

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,10 @@
-import { BadRequestException, Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { AtualizarJogadorDto } from './dtos/atualizar-jogador.dto';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
@@ -7,18 +13,22 @@ import { Jogadores } from './entity/jogadores.entity';
 
 @Injectable()
 export class JogadoresService {
-  constructor(@Inject('JOGADORES_REPOSITORY') private readonly jogadoresRepository: Repository<Jogadores>) {}
+  constructor(
+    @Inject('JOGADORES_REPOSITORY')
+    private readonly jogadoresRepository: Repository<Jogadores>,
+  ) {}
 
   private readonly logger = new Logger(JogadoresService.name);
 
   async criarJogador(criarJogadorDto: CriarJogadorDto): Promise<Resposta> {
-
-    const isjogadorAlreadyCreated = await this.jogadoresRepository.findOneBy({ email: criarJogadorDto.email })
+    const isjogadorAlreadyCreated = await this.jogadoresRepository.findOneBy({
+      email: criarJogadorDto.email,
+    });
 
     if (isjogadorAlreadyCreated) {
       throw new BadRequestException({
         statusCode: 400,
-        message: `Jogador com o email ${criarJogadorDto.email} já existe`,
+        message: `Jogador com o email ${criarJogadorDto.email} jÃÂ¡ existe`,
         data: {},
       });
     }
@@ -30,7 +40,7 @@ export class JogadoresService {
         statusCode: 200,
         message: `Jogador criado com sucesso`,
         data: isJogadorCreated,
-      }
+      };
     }
 
     throw new BadRequestException({
@@ -40,47 +50,55 @@ export class JogadoresService {
     });
   }
 
-  async atualizarJogador(id: number, atualizarJogadorDto: AtualizarJogadorDto): Promise<Resposta> {
+  async atualizarJogador(
+    id: number,
+    atualizarJogadorDto: AtualizarJogadorDto,
+  ): Promise<Resposta> {
     const jogador = await this.jogadoresRepository.findOneBy({ id });
 
     if (!jogador) {
       throw new NotFoundException({
         statusCode: 404,
-        message: `Jogador com o id ${id} não encontrado`,
+        message: `Jogador com o id ${id} nÃÂ£o encontrado`,
         data: {},
       });
     }
 
     const { nome, telefoneCelular } = atualizarJogadorDto;
-    const jogadorUpdated = await this.jogadoresRepository.update(id, { nome, telefoneCelular });
-    
-    if(jogadorUpdated.affected === 0) throw new BadRequestException({
-      statusCode: 400,
-      message: `Erro ao atualizar jogador`,
-      data: {},
+    const jogadorUpdated = await this.jogadoresRepository.update(id, {
+      nome,
+      telefoneCelular,
     });
+
+    if (jogadorUpdated.affected === 0)
+      throw new BadRequestException({
+        statusCode: 400,
+        message: `Erro ao atualizar jogador`,
+        data: {},
+      });
 
     return {
       statusCode: 200,
       message: 'Jogador atualizado com sucesso',
       data: await this.jogadoresRepository.findOneBy({ id }),
-    }
+    };
   }
 
-  async listarJogadores(): Promise<Resposta>{
+  async listarJogadores(): Promise<Resposta> {
     const allJogadores = await this.jogadoresRepository.find();
 
-    if(allJogadores.length <= 0) throw new BadRequestException({
-      statusCode: 400,
-      message: `NÃ£o existem jogadores cadastrados`,
-      data: {}
-    })
+    if (allJogadores.length <= 0)
+      throw new BadRequestException({
+        statusCode: 400,
+        message: `Não existem jogadores cadastrados`,
+        data: {},
+      });
 
     return {
       statusCode: 200,
       message: `Lista de jogadores`,
       data: allJogadores,
-    }
+    };
   }
 
   async apagarJogador(id: number): Promise<Resposta> {
@@ -88,38 +106,45 @@ export class JogadoresService {
     if (!jogador) {
       throw new NotFoundException({
         statusCode: 404,
-        message: `Jogador com o id ${id} nÃ£o encontrado`,
+        message: `Jogador com o id ${id} nÃÂÃÂ£o encontrado`,
         data: {},
       });
     }
 
     const isJogadorDeleted = await this.jogadoresRepository.delete(id);
-    if(isJogadorDeleted.affected === 0) throw new BadRequestException({
-      statusCode: 400,
-      message: `NÃ£o foi possÃ­vel apagar o jogador com o id ${id}`,
-      data: {},
-    })
+    if (isJogadorDeleted.affected === 0)
+      throw new BadRequestException({
+        statusCode: 400,
+        message: `NÃÂÃÂ£o foi possÃÂÃÂ­vel apagar o jogador com o id ${id}`,
+        data: {},
+      });
 
     return {
       statusCode: 200,
       message: 'Jogador apagado com sucesso',
-      data: jogador
-    }
+      data: jogador,
+    };
   }
 
   private async criar(criarJogadorDto: CriarJogadorDto) {
     const { nome, telefoneCelular, email } = criarJogadorDto;
-    const jogadorCreated = await this.jogadoresRepository.save({ nome, telefoneCelular, email });
+    const jogadorCreated = await this.jogadoresRepository.save({
+      nome,
+      telefoneCelular,
+      email,
+    });
     return jogadorCreated;
   }
 
   async consultarJogadorPeloEmail(email: string): Promise<Resposta> {
-    const jogadorEncontrado = await this.jogadoresRepository.findOneBy({ email });
+    const jogadorEncontrado = await this.jogadoresRepository.findOneBy({
+      email,
+    });
 
     if (!jogadorEncontrado) {
       throw new NotFoundException({
         statusCode: 404,
-        message: `Jogador com o email ${email} não encontrado`,
+        message: `Jogador com o email ${email} nÃÂ£o encontrado`,
         data: {},
       });
     }
@@ -128,7 +153,7 @@ export class JogadoresService {
       statusCode: 200,
       message: `Jogador encontrado`,
       data: jogadorEncontrado,
-    }
+    };
   }
 
   async consultarJogadorPeloId(id: number): Promise<Resposta> {
@@ -137,15 +162,15 @@ export class JogadoresService {
     if (!jogador) {
       throw new NotFoundException({
         statusCode: 404,
-        message: `Jogador com o id ${id} não encontrado`,
+        message: `Jogador com o id ${id} nÃÂ£o encontrado`,
         data: {},
-      })
+      });
     }
 
     return {
       statusCode: 200,
       message: `Jogador com o id ${id} encontrado`,
       data: jogador,
-    }
+    };
   }
 }
